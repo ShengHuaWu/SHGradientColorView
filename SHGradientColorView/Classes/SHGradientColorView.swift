97629a5c3306065feb006f0db2d794cd0bf75162
ShengHua Wu <fantasy0404@gmail.com>
2015-05-07 11:49:36 +0800

    Render image in background.

--- SHGradientColorView/Classes/SHGradientColorView.swift
+++ SHGradientColorView/Classes/SHGradientColorView.swift
@@ -15,37 +15,56 @@ enum SHGradientDirection {
 
 class SHGradientColorView: UIView {
     
-    var startColor: UIColor
-    var endColor: UIColor
-    var direction: SHGradientDirection
+    var startColor: UIColor {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    var endColor: UIColor {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    var direction: SHGradientDirection {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    private var backgroundImageView: UIImageView
 
     // MARK: Designated initializer
     init(frame: CGRect, startColor: UIColor, endColor: UIColor, direction: SHGradientDirection) {
         self.startColor = startColor
         self.endColor = endColor
         self.direction = direction
+        self.backgroundImageView = UIImageView(frame: CGRectZero)
         
         super.init(frame: frame)
         self.backgroundColor = UIColor.clearColor()
+        self.addSubview(self.backgroundImageView)
     }
 
     required init(coder aDecoder: NSCoder) {
         self.startColor = UIColor.clearColor()
         self.endColor = UIColor.clearColor()
         self.direction = SHGradientDirection.horizontal
+        self.backgroundImageView = UIImageView(frame: CGRectZero)
         
         super.init(coder: aDecoder)
         self.backgroundColor = UIColor.clearColor()
+        self.addSubview(self.backgroundImageView)
     }
     
-    // MARK: Draw rect
-    override func drawRect(rect: CGRect) {
-        let context = UIGraphicsGetCurrentContext()
-        self.drawGradientInContext(context, forRect: rect)
+    // MARK: Layouts
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.backgroundImageView.frame = self.bounds
+        self.renderImageInBackgroundForRect(self.backgroundImageView.frame)
     }
 
     // MARK: Create gradient
-    func createGradient() -> CGGradient {
+    func createGradient() -> CGGradientRef {
         let colorSpace = CGColorSpaceCreateDeviceRGB()
         let colors: CFArray = [self.startColor.CGColor, self.endColor.CGColor]
         let locations: [CGFloat] = [0.0, 1.0]
@@ -54,13 +73,11 @@ class SHGradientColorView: UIView {
     }
     
     // MARK: Draw gradient
-    func drawGradientInContext(context: CGContext, forRect rect: CGRect) {
+    func drawGradientInContext(context: CGContextRef, forRect rect: CGRect) {
         CGContextSaveGState(context)
-        
         let gradient = self.createGradient()
         let range = self.rangeToDrawInRect(rect)
         CGContextDrawLinearGradient(context, gradient, range.start, range.end, 0)
-        
         CGContextRestoreGState(context)
     }
     
@@ -68,7 +85,6 @@ class SHGradientColorView: UIView {
     func rangeToDrawInRect(rect: CGRect) -> (start: CGPoint, end: CGPoint) {
         var start = CGPointZero
         var end = CGPointZero
-        
         switch self.direction {
         case .horizontal:
             start = CGPointMake(CGRectGetMinX(rect), CGRectGetMidY(rect))
@@ -80,4 +96,20 @@ class SHGradientColorView: UIView {
         
         return (start, end)
     }
+    
+    // Render image
+    func renderImageInBackgroundForRect(rect: CGRect) {
+        NSOperationQueue().addOperationWithBlock { () -> Void in
+            UIGraphicsBeginImageContext(rect.size)
+            let context = UIGraphicsGetCurrentContext()
+            self.drawGradientInContext(context, forRect: rect)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                self.backgroundImageView.image = image
+            })
+        }
+    }
+    
 }
--- SHGradientColorView/ViewController.swift
+++ SHGradientColorView/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var gradientColorView: SHGradientColorView? = nil
 
     // MARK: View life cycle
     override func viewDidLoad() {
@@ -17,7 +19,23 @@ class ViewController: UIViewController {
         let frame = CGRectMake(30, 100, 300, 60)
         let gradientColorView = SHGradientColorView(frame: frame, startColor: UIColor.yellowColor(), endColor: UIColor.blueColor(), direction: SHGradientDirection.horizontal)
         self.view.addSubview(gradientColorView)
+        self.gradientColorView = gradientColorView
+        
+        let button: UIButton = UIButton.buttonWithType(UIButtonType.Custom) as! UIButton
+        button.frame = CGRectMake(150, 200, 80, 44)
+        button.setTitle("Change", forState: UIControlState.Normal)
+        button.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+        button.addTarget(self, action: "changeColor:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(button)
     }
     
+    // MARK: Button action
+    func changeColor(button: UIButton) {
+        if let gradientView = self.gradientColorView {
+//            gradientView.startColor = UIColor.redColor()
+//            gradientView.endColor = UIColor.greenColor()
+            gradientView.direction = SHGradientDirection.vertical
+        }
+    }
 }
 
