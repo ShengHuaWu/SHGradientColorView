afbf8dfeaebab98c8386dd916b2cc2483acaee81
ShengHua Wu <fantasy0404@gmail.com>
2015-05-14 15:53:54 +0800

    Create a framework target.

---
+++ SHGradientColorViewFramework/SHGradientColorViewFramework.h
@@ -0,0 +1,19 @@
+//
+//  SHGradientColorViewFramework.h
+//  SHGradientColorViewFramework
+//
+//  Created by WuShengHua on 5/14/15.
+//  Copyright (c) 2015 ShengHuaWu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SHGradientColorViewFramework.
+FOUNDATION_EXPORT double SHGradientColorViewFrameworkVersionNumber;
+
+//! Project version string for SHGradientColorViewFramework.
+FOUNDATION_EXPORT const unsigned char SHGradientColorViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHGradientColorViewFramework/PublicHeader.h>
+
+
